af0c09506bc097f4b0ade276e33b5e6912b0b639
Clóvis Neto <clovis.neto@liferay.com>
2017-08-17 20:10:32 -0300

    Create dropdown utils, to use the exported variables in the dropdown component

---
+++ src/components/Dropdown/DropdownUtils.js
@@ -0,0 +1,6 @@
+export const ITEMS_DATA = [
+	{id: 0, months: 12, interest:5, name: "Initial"},
+	{id: 1, months: 24, interest:7, name: "Bronze"},
+	{id: 2, months: 36, interest:9, name: "Gold"},
+	{id: 3, months: 48, interest:15, name: "Sphere"}
+];
